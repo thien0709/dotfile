05a05ffd556fb8217dedd6aa750056bab29d61ed
thien0709 <thien070904@gmail.com>
2025-06-10 23:24:53 +0700

    fix : lazyvim gruvbox, polybar

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,31 @@
 return {
-	"folke/tokyonight.nvim",
+	"ellisonleao/gruvbox.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		require("tokyonight").setup({
-			style = "night",
-			transparent = true,
-			-- terminal_colors = true,
-			styles = {
-				sidebars = "dark",
-				floats = "dark",
-				comments = { italic = true },
-				keywords = { italic = true },
-				functions = { italic = true },
-				variables = { italic = true },
-			},
-		})
-		vim.cmd.colorscheme("tokyonight-night")
+		require("gruvbox").setup({
+  			terminal_colors = true, -- add neovim terminal colors
+  			undercurl = true,
+  			underline = true,
+  			bold = true,
+  			italic = {
+   				strings = true,
+ 				emphasis = true,
+    				comments = true,
+    				operators = false,
+    				folds = true,
+  			},
+  			strikethrough = true,
+ 			invert_selection = false,
+  			invert_signs = false,
+  			invert_tabline = false,
+  			inverse = true, -- invert background for search, diffs, statuslines and errors
+  			contrast = "", -- can be "hard", "soft" or empty string
+  			palette_overrides = {},
+  			overrides = {},
+  			dim_inactive = false,
+  			transparent_mode = true,
+			})
+		vim.cmd("colorscheme gruvbox")
 	end,
 }
